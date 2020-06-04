3862996b21db66efaa2d3259fbf4bdec213be8a0
rogerio carvalho <rogerio.carvalho.silva15@gmail.com>
2020-06-04 08:40:56 -0300

    DONT COMMIT YOUR TOKEN

--- data_collector/src/utils/Tokens.java
+++ data_collector/src/utils/Tokens.java
@@ -3,7 +3,7 @@
 public class Tokens {
 
 	public static final String USERNAME = "CARVALHO7976";
-	public static final String PASSWORD = "37853ab6571c9780e1567509971a8162d21b46a7";
+	public static final String PASSWORD = "DONT COMMIT YOUR TOKEN, REMEMBER :D";
 	
 	//public static final String USERNAME = "andersonss";
 	//public static final String PASSWORD = "06746c92deb866b34f444e6150b2078b31fcca5e";
